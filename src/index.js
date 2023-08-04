17759f833e1b4a9bd905cfd7e5707abae121d11e
Horbunov Yurii <urijnet@gmail.com>
2023-08-04 09:57:34 +0300

    fix-css-and-scroll

--- src/index.js
+++ src/index.js
@@ -130,14 +130,14 @@ async function onLoadMoreBtnClick(){
    const data = await fetchImages()
     renderCard(data);
 
-    // const { height: cardHeight } = document
-    //   .querySelector('.gallery')
-    //   .firstElementChild.getBoundingClientRect();
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
 
-    // window.scrollBy({
-    //   top: cardHeight * 2,
-    //   behavior: 'smooth',
-    // });
+    window.scrollBy({
+      top: cardHeight * 2,
+      behavior: 'smooth',
+    });
 
     setStateLoadButton(data);
 }
